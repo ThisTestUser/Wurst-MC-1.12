3c1b601fa41274519b8c167663c43adef358325c
ThisTestUser <ThisTestUser@users.noreply.github.com>
2017-07-01 18:37:41 -0400

    Update DisableSpf.java

--- shared-src/net/wurstclient/features/special_features/DisableSpf.java
+++ shared-src/net/wurstclient/features/special_features/DisableSpf.java
@@ -1,5 +1,5 @@
 /*
- * Copyright � 2014 - 2017 | Wurst-Imperium | All rights reserved.
+ * Copyright © 2014 - 2017 | Wurst-Imperium | All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -14,10 +14,17 @@ import net.wurstclient.features.Spf;
 	"vanilla Minecraft"})
 public final class DisableSpf extends Spf
 {
+	public final ModeSetting enableMode =
+		new ModeSetting("Mode", new String[]{"Button", "Code", "Restart"}, 0);
+		
 	public DisableSpf()
 	{
 		super("Disable Wurst",
-			"To disable Wurst, go to the Statistics screen and press the \"Disable Wurst\" button. It will turn\n"
-				+ "into an \"Enable Wurst\" button once pressed.");
+			"To disable Wurst, go to the Statistics screen and press the \"Disable Wurst\" button.\n"
+				+ "There are 3 modes for re-enabling:\n"
+				+ "§Button§r mode adds an Enable Wurst button in the Statistics screen.\n"
+				+ "§Code§r mode generates a code that you have to type to re-enable.\n"
+				+ "§Restart§r mode forces a restart to reenable the client.");
+		addSetting(enableMode);
 	}
 }
